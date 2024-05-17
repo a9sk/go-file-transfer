88c1dde7b9dc9cabafc0f7795c3068a9fc3ae7fa
a9sk <920a9sk42f76c765@proton.me>
2024-05-17 18:56:14 +0200

    client skeletron added

--- client/client.go
+++ client/client.go
@@ -1,1 +1,32 @@
+// client/client.go
+
 package client
+
+import (
+	"crypto/tls"
+	"fmt"
+)
+
+type Client struct {
+	Host string
+	Port string
+	// client-specific stuff and configurations should go here
+}
+
+func NewClient(host, port string) *Client {
+	return &Client{
+		Host: host,
+		Port: port,
+	}
+}
+
+func (c *Client) Connect() (*tls.Conn, error) {
+	// connect
+	conn, err := tls.Dial("tcp", c.Host+":"+c.Port, &tls.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to server: %v", err)
+	}
+	return conn, nil
+}
+
+// other methods for sending files, handling responses, etc.
