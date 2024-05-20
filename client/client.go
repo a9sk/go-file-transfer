4ba8c899dcd7f1a9701315b504e3e57f1c0ffc9d
a9sk <920a9sk42f76c765@proton.me>
2024-05-20 19:30:31 +0200

    tls certificate check added

--- client/client.go
+++ client/client.go
@@ -21,11 +21,21 @@ func NewClient(host, port string) *Client {
 }
 
 func (c *Client) Connect() (*tls.Conn, error) {
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: true, // Skip certificate verification
+	}
 	// connect
-	conn, err := tls.Dial("tcp", c.Host+":"+c.Port, &tls.Config{})
+	conn, err := tls.Dial("tcp", c.Host+":"+c.Port, tlsConfig)
 	if err != nil {
 		return nil, fmt.Errorf("%v", err)
 	}
+	fmt.Println("[debug] We did get here eventually")
+	if err := VerifyServerCertificate(conn); err != nil {
+		fmt.Println("[!] Closing the connection...")
+		conn.Close()
+		return nil, err
+	}
+
 	return conn, nil
 }
 
--- client/utils.go
+++ client/utils.go
@@ -1,1 +1,36 @@
 package client
+
+import (
+	"crypto/tls"
+	"fmt"
+)
+
+func VerifyServerCertificate(conn *tls.Conn) error {
+	fmt.Println("[debug] We also got here ggs")
+	// check if certificate is provided
+	if len(conn.ConnectionState().PeerCertificates) == 0 {
+		return fmt.Errorf("server did not provide a certificate")
+	}
+
+	if len(conn.ConnectionState().VerifiedChains) == 0 || len(conn.ConnectionState().VerifiedChains[0]) == 0 || !conn.ConnectionState().VerifiedChains[0][0].IsCA {
+		fmt.Println("[!] Server's certificate is not signed by a trusted CA.")
+		fmt.Println("[?] Do you want to proceed and trust the server? (yes/no): ")
+		var resp string
+
+		for {
+			fmt.Scanln(&resp)
+			switch resp {
+			case "yes":
+				fmt.Println("[*] Proceeding with the connection...")
+				return nil
+			case "no":
+				return fmt.Errorf("connection aborted by user")
+			default:
+				fmt.Println("[*] Please enter 'yes' or 'no'")
+			}
+		}
+	}
+	fmt.Println("[*] Best case, Server's certificate is signed by a trusted CA ;)")
+
+	return nil
+}
