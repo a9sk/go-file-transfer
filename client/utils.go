7168ac48a9b473414c31d555fb4a678275641ef6
a9sk <920a9sk42f76c765@proton.me>
2024-05-22 22:26:25 +0200

    client send file to server

--- client/utils.go
+++ client/utils.go
@@ -3,6 +3,8 @@ package client
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 )
 
@@ -48,11 +50,52 @@ func FileTransfer(conn *tls.Conn) error {
 		switch arrayUserInput[0] {
 		case "send":
 			fmt.Println("[*] Seems like you are trying to send a file to the server...")
-
+			sendFilesToServer(arrayUserInput[1], conn)
 		case "get":
 			fmt.Println("[*] Seems like you are trying to get a file from the server...")
 		default:
 			fmt.Println("[!] Invalid syntax, insert 'send' or 'get' to send or get files to or from the server")
 		}
 	}
+}
+
+func sendFilesToServer(fileName string, conn *tls.Conn) error {
+	defer conn.Close()
+
+	fileBuffer := make([]byte, BUFFER_SIZE)
+
+	// imagine sending a non existing file...
+	_, err := os.Stat(strings.TrimSpace(fileName))
+	if os.IsNotExist(err) {
+		conn.Write([]byte("-1"))
+		return fmt.Errorf("[!] The file does not exist")
+	}
+
+	file, err := os.Open(strings.TrimSpace(fileName))
+	if err != nil {
+		conn.Write([]byte("-1"))
+		return fmt.Errorf("[!] Could not open the file")
+	}
+	defer file.Close()
+
+	_, err = conn.Write([]byte("send " + fileName))
+	if err != nil {
+		return fmt.Errorf("[!] Could not write the connection bytes")
+	}
+
+	// read the file buffer and send it until it "exists"
+	for {
+		n, err := file.Read(fileBuffer)
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("[!] Cannot read the filebufffer")
+		}
+		if n == 0 {
+			break
+		}
+		_, err = conn.Write(fileBuffer[:n])
+		if err != nil {
+			return fmt.Errorf("[!] Impossible to write the filebuffer")
+		}
+	}
+	return nil
 }
