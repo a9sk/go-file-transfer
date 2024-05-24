b30275fb28ce0f41d7b1884b8626e63c2888680e
a9sk <920a9sk42f76c765@proton.me>
2024-05-24 18:56:55 +0200

    server sends files method added

--- server/utils.go
+++ server/utils.go
@@ -5,9 +5,10 @@ import (
 	"io"
 	"net"
 	"os"
+	"strings"
 )
 
-func GetFileFromClient(fileName string, conn net.Conn) error {
+func getFilesFromClient(fileName string, conn net.Conn) error {
 	defer conn.Close()
 	// try n create a file on the server
 	//file, err := os.Create(fileName)
@@ -25,5 +26,29 @@ func GetFileFromClient(fileName string, conn net.Conn) error {
 	fmt.Printf("[*] Received %d bytes and saved to %s\n", n, fileName)
 
 	conn.Close()
+	return nil
+}
+
+func sendFilesToClient(fileName string, conn net.Conn) error {
+	// imagine sending a non existing file...
+	file, err := os.Open(strings.TrimSpace(fileName))
+	if err != nil {
+		conn.Write([]byte("[!] The file does not exist..."))
+		return fmt.Errorf("[!] Could not open the file")
+	}
+	defer file.Close()
+
+	_, err = conn.Write([]byte(fileName))
+	if err != nil {
+		return fmt.Errorf("[!] Could not write the file name")
+	}
+
+	n, err := io.Copy(conn, file)
+	if err != nil {
+		return fmt.Errorf("[!] Error while copying file contents: %v", err)
+	}
+
+	fmt.Printf("[debug] %d bytes sent", n)
+
 	return nil
 }
